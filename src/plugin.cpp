36674c7eeebb7a6c10c644350e4487f14ea0ee21
DNKpp <dnkpp2011@gmail.com>
2019-12-28 16:07:08 +0100

    store path global
    Signed-off-by: DNKpp <dnkpp2011@gmail.com>

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -8,6 +8,8 @@
 #include <cassert>
 #include <iostream>
 #include <string_view>
+#include <stdexcept>
+#include <charconv>
 
 #include "Vector.hpp"
 
@@ -18,6 +20,8 @@ bool is_valid_pos(const Vector& _at)
 	return 0 <= _at.x && _at.x < map.getWidth() &&
 		0 <= _at.y && _at.y < map.getHeight();
 }
+
+static std::optional<std::vector<Vector>> stored_path;
 
 class Pathfinder
 {
@@ -34,7 +38,16 @@ public:
 				return _lhs.x < _rhs.x || (_lhs.x == _rhs.x && _lhs.y < _rhs.y);
 			}
 		};
-		auto searcher = sl::graph::make_breadth_first_searcher<Vector>();
+
+		//auto searcher = sl::graph::make_breadth_first_searcher<Vector>();
+
+		auto costCalculator = [](const Vector& _prevPos, const Vector& _pos) { return 1; }; // ToDo: calculate cost from terrain type
+		auto heuristicCalculator = [](const Vector& _prevPos, const Vector& _pos)
+		{
+			auto diff = _pos - _prevPos;
+			return diff.x + diff.y;
+		};
+		auto searcher = sl::graph::make_astar_searcher<Vector>(costCalculator, heuristicCalculator);
 		using Searcher_t = decltype(searcher);
 		using Map_t = std::map<typename Searcher_t::VertexType, typename Searcher_t::NodeDataType, VectorLess>;
 		using NodeMap = sl::graph::NodeMap<Map_t>;
@@ -57,14 +70,64 @@ public:
 					_callback(at);
 			}
 		};
+
 		auto path = sl::graph::find_path(start, _end, neighbourSearcher, searcher, NodeMap(Map_t(VectorLess())), NodeMap(Map_t(VectorLess())));
 		if (path)
+		{
+			stored_path = std::move(path);
 			return std::size(*path);
+		}
 		return -1;
 	}
 	
 private:
 	
+};
+
+struct Param
+{
+	static std::optional<int> get_integer(const RPG::ParsedCommentParameter& _param)
+	{
+		switch (_param.type)
+		{
+		case RPG::PARAM_NUMBER:
+			return get_from_number(_param);
+		case RPG::PARAM_TOKEN:
+		
+		default:
+			throw std::runtime_error("Invalid argument.");
+		}
+	}
+
+private:
+	static std::optional<int> get_from_number(const RPG::ParsedCommentParameter& _param)
+	{
+		auto index = static_cast<int>(_param.number);
+		if (0 <= index) // remark: no upper bound check necessary?
+			return RPG::variables[index];
+		return std::nullopt;
+		//throw std::runtime_error("Variables index out of bounds.");
+	}
+
+	static std::optional<int> get_from_token(const RPG::ParsedCommentParameter& _param)
+	{
+		std::string_view token{ _param.text };
+		if (token[0] == 'V')
+		{
+			auto valBegin = std::find_if_not(std::rbegin(token), std::rend(token), [](char _c) { return _c == 'V'; }).base();
+			int curIndex = 0;
+			auto result = std::from_chars(&*valBegin, token.data() + token.size(), curIndex);
+		    if (result.ec == std::errc::invalid_argument)
+		        throw std::runtime_error("Invalid token.");
+
+			for (auto itr = std::begin(token); itr != valBegin; ++itr)
+				curIndex = RPG::variables[curIndex];
+
+			if (0 <= curIndex) // remark: no upper bound check necessary?
+				return RPG::variables[curIndex];
+		}
+		throw std::runtime_error("Variables index out of bounds.");
+	}
 };
 
 // called on comment
@@ -77,7 +140,32 @@ bool onComment(const char* _text, const RPG::ParsedCommentData* _parsedData, RPG
 		auto& params = _parsedData->parameters;
 		Pathfinder p;
 		auto length = p.calc_path({ 20, 20 });
-		RPG::variables[1] = length;
+		auto& outLength = Integer::get(params[0]);
+		outLength = length;
+		return false;
+	}
+	else if (cmd == "get_path_length")
+	{
+		auto& params = _parsedData->parameters;
+		auto id = params[0].number;
+		auto& outLength = Integer::get(params[1]);
+
+		auto& path = *stored_path;
+		outLength = std::size(path);
+		return false;
+	}
+	else if (cmd == "get_path_vertex")
+	{
+		auto& params = _parsedData->parameters;
+		auto id = params[0].number;
+		auto index = params[1].number;
+		auto& outX = Integer::get(params[2]);
+		auto& outY = Integer::get(params[3]);
+
+		auto& path = *stored_path;
+		auto vertex = path[static_cast<std::size_t>(index)];
+		outX = vertex.x;
+		outY = vertex.y;
 		return false;
 	}
 	return true;
