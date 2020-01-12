6405d2c084969efd1a213bca7e587a378cf10690
DNKpp <dnkpp2011@gmail.com>
2020-01-12 15:42:22 +0100

    update documentation
    Signed-off-by: DNKpp <dnkpp2011@gmail.com>

--- src/_Documentation.h
+++ src/_Documentation.h
@@ -27,6 +27,70 @@
 	won't be touched when the functions fails.</li>
 	</ul>
 
-	\code @find_path out_id, out_s_success
+	\subsection Interface
+
+	\subsubsection find_path Find Path
+	\code @find_path in_target, in_x, in_y out_path_id, out_s_success
 	\endcode
+
+	@param in_target Accepts event ids as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Accepts also specific tokens for special events (hero, airship, ship, skiff).
+	@param in_x Accepts x-coordinate as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...).
+	@param in_y Accepts y-coordinate as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...).
+	@param out_path_id Accepts an id for RPG::variables as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will insert the id of the newly generated path in the variable at the provided index.
+	@param out_s_success Accepts id for RPG::switches as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will use the provided value as switch index to hand back the result of the function (false = it failed; true = success).
+
+
+	\subsubsection get_path_length Get Path Length
+	\code @get_path_length in_path_id, out_path_length, out_s_success
+	\endcode
+
+	@param in_path_id Accepts path ids as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the path.
+	@param out_length Accepts an id for RPG::variables as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will insert the length of the path at the passed index.
+	@param out_s_success Accepts id for RPG::switches as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will use the provided value as switch index to hand back the result of the function (false = it failed; true = success).
+
+
+	\subsubsection get_path_vertex Get Path vertex
+	\code @get_path_vertex in_path_id, in_vertex_index, out_vertex_x, out_vertex_y, out_s_success
+	\endcode
+
+	@param in_path_id Accepts path ids as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the path.
+	@param in_vertex_index Accepts vertex index (starting at 0) as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the path of which you will get the length for.
+	@param out_vertex_x Accepts an id for RPG::variables as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will insert the x-coordinate value of the vertex at the provided index.
+	@param out_vertex_y Accepts an id for RPG::variables as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will insert the y-coordinate value of the vertex at the provided index.
+	@param out_s_success Accepts id for RPG::switches as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will use the provided value as switch index to hand back the result of the function (false = it failed; true = success).
+
+
+	\subsubsection clear_path Clear Path
+	\code @clear_path in_path_id
+	\endcode
+
+	@param in_path_id Accepts path ids as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the path.
+
+
+	\subsubsection set_terrain_cost Set Terrain Cost
+	\code @set_terrain_cost in_terrain_id, in_cost
+	\endcode
+
+	@param in_terrain_id Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the terrain id the cost will be used for.
+	@param in_terrain_id Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). This will be used as cost of the given terrain id.
+
+
+	\subsubsection reset_terrain_cost Reset Terrain Cost
+	\code @reset_terrain_cost in_terrain_id
+	\endcode
+
+	@param in_terrain_id Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the terrain id to be reset.
+
+
+	\subsubsection clear_terrain_costs Clear Terrain Costs
+	\code @clear_terrain_costs
+	\endcode
+
+
+	\subsubsection get_terrain_cost Get Terrain Cost
+	\code @get_terrain_cost in_terrain_id, out_cost
+	\endcode
+
+	@param in_terrain_id Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the terrain id to be reset.
+	@param out_cost Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will insert the cost of the terrain at the passed index.
 */
