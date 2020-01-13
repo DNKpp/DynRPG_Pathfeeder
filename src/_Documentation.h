6e8da55501b289d9c7b2a8686eb7eeb31c91d340
DNKpp <dnkpp2011@gmail.com>
2020-01-13 23:41:12 +0100

    update documentation
    Signed-off-by: DNKpp <dnkpp2011@gmail.com>

--- src/_Documentation.h
+++ src/_Documentation.h
@@ -1,19 +1,25 @@
 /*!
-	\mainpage About DynRPG_Pathfinder
+	\mainpage About DynRPG_Pathfeeder
 
 	\author Dominic "DNKpp" Koepke
 
 	\section download Download
 	<b><a href=""></a></b>
 
+	\section source Source
+	The complete source code is available at:
+	<b>https://github.com/DNKpp/DynRPG_Pathfinder<a href="https://github.com/DNKpp/DynRPG_Pathfinder"></a></b>
+
 	\section contact Contact
-	If you encounter any issues or bugs, feel free to contact me at <b><a href="DNKpp2011@gmail.com"></a></b>.
+	If you encounter any issues or bugs, feel free to contact me at <b>DNKpp2011@gmail.com<a href="DNKpp2011@gmail.com"></a></b>.
 
 	\section introduction Introduction
 	This is a plugin written for the DynRPG 0.32. It is not possible to use it with previous versions like DynRPG 0.20, because the plugin makes
 	heavy usage of functions, which were introduced later on.
 	At first: Unlike my previous pathfinder plugin, I don't move any character myself. You have to handle the movement yourself. The plugin just
 	hands over the information you'll need.
+
+	<hr>
 
 	\section documentation Documentation
 	Before we start with the function documentation itself, please read the following guidelines first:
@@ -25,7 +31,24 @@
 	<li>If any of those functions have a parameter named "out_s_success" they are able to fail. When referring to any other out variable of that function, you <b>must</b> check the success
 	variable first to be sure to receive valid values. The success variable is the only parameter which will be either set to true or false. If the function fails, any other out parameter
 	won't be touched when the functions fails.</li>
+	<li>Most (if not all) parameters of the functions in this plugin are integers. To save some space and keep the documentation clean please keep in mind, that you are able to either pass a plain value (like 1, 2, -2, ...) or a indirect value
+		through a RPG::variable (v1 refers to the RPG::variable with id 1; v2 to id 2; etc.). You are able to chain those leading 'v', thus you are able to perform multiple indirections (vv1 is looking into variable 1 and using this value as the final id).
+		It is important to keep in mind, that, if not stated otherwise, ids starting at 1 (not 0).
 	</ul>
+
+	\subsection Concept
+	This plugin consists of two parts: the configuration and the "pathfeeding".
+
+	Let's start with the interesting one. What is "pathfeeding" and how does this help you? As already stated in the introduction, this plugin won't touch your events. This plugin will simply calculate the best available path from one event to
+	a given destination. You are able to retrieve any information you will need to move your event; but it is up to you, how you do that. Like already mentioned, you trigger the pathfinding via event comment and after that you are able to
+	pull the information for the next step out of the plugin when you need it; again via event comment. If you still don't understand, there will be a example Game attached to the download.
+	One thing to be aware of: Each path you'll generate will be stored internally as long as you do not clear it via event comment or change the map. Every path stores a 4 byte size, and for each of its vertices, another 8 bytes. This means, if you generate
+	a path of 10 vertices you will consume 84 bytes of memory. If you do that often it quickly sums up to a huge amount of memory. For this reason it's important, especially when your game only runs on one map, to clear each path when you are done with it. 
+
+	The second part is about configuring the plugin. Currently you are only able to configure the cost of each terrain id. When you leave everything untouched the plugin will use the terrain id of each tile as its cost value, but you are able to
+	manipulate this behavior via DynRPG.ini and, again, event comment. The configuration via ini will be initially loaded when you start a new game. Every other modification of those value will be serialized to the save files.
+
+	<hr>
 
 	\subsection Interface
 
@@ -37,11 +60,12 @@
 
 	\attention This function may fail if the destination is not reachable. Please always check the success information before relying to the other output.
 
-	@param in_target Accepts event ids as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Accepts also specific tokens for special events (hero, airship, ship, skiff).
-	@param in_x Accepts x-coordinate as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...).
-	@param in_y Accepts y-coordinate as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...).
-	@param out_path_id Accepts an id for RPG::variables as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will insert the id of the newly generated path in the variable at the provided index.
-	@param out_s_success Accepts id for RPG::switches as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will use the provided value as switch index to hand back the result of the function (false = it failed; true = success).
+	@param in_target Accepts event ids in integer style. Accepts also specific tokens for special events (hero, airship, ship, skiff).
+	@param in_x Expects x-coordinate in integer style.
+	@param in_y Expects y-coordinate in integer style.
+	@param out_path_id Expects an RPG::variable id in integer style. It will insert the id of the newly generated path in the variable at the provided index.
+	@param out_s_success Expects RPG::switch id in integer style. It will use the provided value as switch index to hand back the result of the function (false = it failed; true = success).
+
 
 
 	\subsubsection get_path_length Get Path Length
@@ -52,9 +76,10 @@
 
 	\attention This function may fail if the path is not available. Please always check the success information before relying to the other output.
 
-	@param in_path_id Accepts path ids as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the path.
-	@param out_length Accepts an id for RPG::variables as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will insert the length of the path at the passed index.
-	@param out_s_success Accepts id for RPG::switches as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will use the provided value as switch index to hand back the result of the function (false = it failed; true = success).
+	@param in_path_id Expects path ids in integer style. Identifies the path.
+	@param out_length Expects an RPG::variable id in integer style. It will insert the length of the path at the passed index.
+	@param out_s_success Expects RPG::switch id in integer style. It will use the provided value as switch index to hand back the result of the function (false = it failed; true = success).
+
 
 
 	\subsubsection get_path_vertex Get Path vertex
@@ -66,20 +91,20 @@
 
 	\attention This function may fail if the path is not available or the vertex index is out of bounds. Please always check the success information before relying to the other output.
 
-	@param in_path_id Accepts path ids as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the path.
-	@param in_vertex_index Accepts vertex index (starting at 0) as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the path of which you will get the length for.
-	@param out_vertex_x Accepts an id for RPG::variables as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will insert the x-coordinate value of the vertex at the provided index.
-	@param out_vertex_y Accepts an id for RPG::variables as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will insert the y-coordinate value of the vertex at the provided index.
-	@param out_s_success Accepts id for RPG::switches as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will use the provided value as switch index to hand back the result of the function (false = it failed; true = success).
+	@param in_path_id Expects path ids in integer style. Identifies the path.
+	@param in_vertex_index Expects vertex index (starting at 0) in integer style. Identifies the vertex you will get the components from.
+	@param out_vertex_x Expects an RPG::variable id in integer style. It will insert the x-coordinate value of the vertex at the provided index.
+	@param out_vertex_y Expects an RPG::variable id in integer style. It will insert the y-coordinate value of the vertex at the provided index.
+	@param out_s_success Expects RPG::switch id in integer style. It will use the provided value as switch index to hand back the result of the function (false = it failed; true = success).
 
 
 	\subsubsection clear_path Clear Path
 	\code @pathfeeder_clear_path in_path_id
 	\endcode
 
-	@param in_path_id Accepts path ids as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the path.
-
 	\details Every path you generated via this plugin will be stored internally to be able to provide the needed information when necessary. When you are done with the path, you should clear it to free memory for further tasks.
+
+	@param in_path_id Accepts path ids as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the path.
 
 
 	\subsubsection set_terrain_cost Set Terrain Cost
@@ -88,8 +113,8 @@
 
 	\details The cost for the specified terrain will be used for future path finding tasks. It will be cached internally until you reset the specific terrain or clear everything.
 
-	@param in_terrain_id Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the terrain id the cost will be used for.
-	@param in_cost Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). This will be used as cost of the given terrain id.
+	@param in_terrain_id Expects terrain id in integer style. Identifies the terrain id the cost will be used for.
+	@param in_cost Expects value in integer style. This will be used as cost of the given terrain id.
 
 
 	\subsubsection set_terrain_cost_var Set Terrain Cost Var
@@ -98,8 +123,8 @@
 
 	\details The RPG::variable id value will be used as dynamical cost for the specified terrain for future path finding tasks.
 
-	@param in_terrain_id Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the terrain id the cost will be used for.
-	@param in_var_id Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). This will be used as an id to a RPG::variable, which holds the cost of the given terrain id.
+	@param in_terrain_id Expects terrain id in integer style. Identifies the terrain id the cost will be used for.
+	@param in_var_id Expects an RPG::variable id in integer style. This will be used to dynamically lookup the cost value of the given terrain id.
 
 
 	\subsubsection reset_terrain_cost Reset Terrain Cost
@@ -108,14 +133,14 @@
 
 	\details The cost for the specified terrain will be reset to default (terrain id as cost).
 
-	@param in_terrain_id Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the terrain id to be reset.
+	@param in_terrain_id Expects terrain id in integer style. Identifies the terrain id to be reset.
 
 
 	\subsubsection clear_terrain_costs Clear Terrain Costs
 	\code @pathfeeder_clear_terrain_costs
 	\endcode
 
-	\details Clears every cached cost value.
+	\details Clears every cached cost value or variable.
 
 
 	\subsubsection get_terrain_cost Get Terrain Cost
@@ -124,6 +149,14 @@
 
 	\details Returns the current cached terrain cost into the RPG::variable with the specified id.
 
-	@param in_terrain_id Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). Identifies the terrain id to be reset.
-	@param out_cost Accepts value as plain number (1, 02, 500, ...) or as variable (v1, vV2, V3, ...). It will insert the cost of the terrain at the passed index.
+	@param in_terrain_id Expects terrain id in integer style. Identifies the terrain id to be reset.
+	@param out_cost Expects an RPG::variable id in integer style. It will insert the cost of the terrain at the passed id
+
+	<hr>
+
+
+	\subsection technical_details Technical Details
+	Due to the antique version of the gcc used by all the other plugins I tried my best to get around this. To be honest gcc was a pain to me, so I tried 2 other compilers: msvc and clang. Due to the different asm syntax I wasn't able to compile
+	the library with msvc; but clang did the job. As a result I was able to update the c++ version to c++17, which offers some huge conveniences. After some progress I realized, that clangs optimizer was to aggressive and simply kicked some code
+	from the binary, because it thought this wasn't necessary. But I was finally able to tell it via compiler flag <b>/Oy-</b> to be more patient and now it seems fine. This means, if you encounter anything weird, please don't hesitate to contact me.
 */
